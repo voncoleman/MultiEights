0d9733e6214d0bce429a64de4c3a70582bb76fd9
Von Coleman <von.coleman@yahoo.com>
2023-10-12 21:54:25 -0500

    first commit

---
+++ src/MultiEights.java
@@ -0,0 +1,15 @@
+public class MultiEights { //Class name is MultiEights, and it's public.
+    public static void main(String[] args) { //The beginning of the program.
+        System.out.println(8 * 1); //Ctrl C, Ctrl V are your friends. Train your fingers, smash those keys!
+        System.out.println(8 * 2);
+        System.out.println(8 * 3);
+        System.out.println(8 * 4);
+        System.out.println(8 * 5);
+        System.out.println(8 * 6);
+        System.out.println(8 * 7);
+        System.out.println(8 * 8);
+        System.out.println(8 * 9);
+        System.out.println(8 * 10);
+
+    }
+}
